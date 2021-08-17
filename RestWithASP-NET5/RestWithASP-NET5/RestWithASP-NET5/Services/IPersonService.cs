72b49ca8c067b5de97535944291695a129c932d3
depaula010 <rafael.paula@netviewinformatica.com.br>
2021-08-17 20:38:20 -0300

    Person

---
+++ RestWithASP-NET5/RestWithASP-NET5/RestWithASP-NET5/Services/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASP_NET5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindBy(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}

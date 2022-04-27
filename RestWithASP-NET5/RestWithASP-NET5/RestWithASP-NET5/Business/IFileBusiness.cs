952c19f815fa3e1de0dc4eb65d5547f1e035951f
depaula010 <rafael.paula@netviewinformatica.com.br>
2022-04-27 19:59:01 -0300

    trabalhando com arquivos

---
+++ RestWithASP-NET5/RestWithASP-NET5/RestWithASP-NET5/Business/IFileBusiness.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASP_NET5.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string fileName);
+        public Task<FileDatailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDatailVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
